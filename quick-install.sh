465573e98fc5d734de30720e9aa6069f3e0d3d3d
jahlib <60108339+jahlib@users.noreply.github.com>
2026-05-07 13:26:22 +0300

    Update quick-install.sh

--- quick-install.sh
+++ quick-install.sh
@@ -5,7 +5,7 @@ set -e
 INSTALL_DIR="/opt/sing-box-warp"
 CONFIG_DIR="/etc/sing-box-warp"
 CACHE_DIR="/var/cache/sing-box-warp"
-SING_BOX_VERSION="1.12.12-extended-1.5.1"
+SING_BOX_VERSION="1.13.2-extended-1.6.2"
 SING_BOX_URL="https://github.com/shtorm-7/sing-box-extended/releases/download/v${SING_BOX_VERSION}/sing-box-${SING_BOX_VERSION}-linux-amd64.tar.gz"
 
 echo "=== Sing-Box WARP Quick Installer ==="
@@ -23,6 +23,7 @@ mkdir -p "$CACHE_DIR"
 
 echo ""
 echo "=== WARP Configuration ==="
+echo "goto ->> https://generator-warp-config.netlify.app/ generater for throne"
 read -p "Enter your warp.conf URL (wg://...): " WARP_URL < /dev/tty
 
 if [ -z "$WARP_URL" ]; then
